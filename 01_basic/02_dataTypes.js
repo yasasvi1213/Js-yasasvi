22f131212e64bcd5d809c9afc6f1f4ca72ad949e
YASASVI THADIBOYINA <yasasvi6chinnu@gmail.com>
2023-11-22 17:02:28 +0530

    basic test

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"Use strict"; // treats all the code as newer version
+
+// alert('Hello') // we are using node js not browser
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined =>
+// symbol  => unique (used for unique)
+
+// object
+
+console.log(typeof 'name');// string
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
